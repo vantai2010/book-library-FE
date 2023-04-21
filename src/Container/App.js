219e833a488a3a1cd7f9f69b5aee3d799e15226f
vantai2010 <94180923+vantai2010@users.noreply.github.com>
2023-04-21 14:24:18 +0700

    Update App.js

--- src/Container/App.js
+++ src/Container/App.js
@@ -50,7 +50,7 @@ function App() {
           <Route path='/extra-infor-user' element={<ExtraInforUser />} />
           <Route path='/history-transaction' element={<HistoryTransaction />} />
           <Route path='/infor-author/:id' element={<InforAuthor />} />
-          <Route path="/register/extra-infor/:email/:language"  element={<EnterInforUser />} />
+          <Route path="/register/extra-infor"  element={<EnterInforUser />} />
           <Route path="/auth/forgot-password"  element={<HandleForgotPassword />} />
           <Route exact path='/system' element={<System />} >
             <Route path="manage-borrow" element={<ManageBorrow />} />
